5b1a819c57df03fc74d83d4239b3c2d7150236fb
menti <gmtysk321@gmail.com>
2019-10-05 13:41:38 +0900

    still_haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  config.generators do |g|
+    g.test_framework false
+    g.helper false
+    g.javascripts false
+  end
 end
