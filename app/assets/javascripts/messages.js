b875c8a223bd4216197dc0349970c6973044c4c0
menti <gmtysk321@gmail.com>
2020-01-29 00:42:50 +0900

    インクリメンタルサーチの実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -39,7 +39,7 @@ $(function(){
         return html;
       };
     }   
-$('.js-form').on('submit', function(){
+$('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -54,7 +54,7 @@ $('.js-form').on('submit', function(){
      .done(function(data){
        var html = buildHTML(data);
        $('.messages').append(html); 
-       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');     
+       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');     
        $('form')[0].reset();
      })
       .fail(function(){
